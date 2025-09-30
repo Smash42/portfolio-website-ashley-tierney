0b4305580064e45faadb1fe2089320c966e5dfb5
Smash42 <ashley64292@cccneb.edu>
2025-09-30 18:28:46 -0400

    Initial
    Initial upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# portfolio-website-ashley-tierney
+# portfolio-tierney
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+"# portfolio-website-ashley-tierney" 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home backdrop">
+    <h1> Ashley Tierney Portfolio</h1>
+      <section style="flex:auto"> image Insert
+      </section>
+        <section>
+          <h3>About Me: </h3>
+          <p> An aspiring web developer, who is taking the time to go 
+            back to school whilst working full time. Diligently working on learning all different programming
+            languages to better suit the development world.  
+            Sharpening my skills and continuing my education everyday to prepare myself.
+          Looking to create resposive apps and web pages to easily align with the consumer needs.     </p>
+        </section>     
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
+<style>
+.backdrop {
+    width: 100%;
+    height: 100%;
+    background: rgb(245, 255, 139);
+}
+</style>
