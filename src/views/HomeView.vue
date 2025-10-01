2c1a239181b1146dcf72fda09f117e55b1da9e68
Smash42 <ashley64292@cccneb.edu>
2025-10-01 10:38:14 -0400

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="home backdrop">
-    <h1> Ashley Tierney Portfolio</h1>
-      <section style="flex:auto"> image Insert
-      </section>
+  <h1 class="header"> Ashley Tierney Portfolio</h1>
+  <div class="home backdrop homepage-contain">
+    <div class="images">
+        <section> image Insert
+        </section>
+    </div >
+    <div class="info">
         <section>
           <h3>About Me: </h3>
-          <p> An aspiring web developer, who is taking the time to go 
+            <p> An aspiring web developer, who is taking the time to go 
             back to school whilst working full time. Diligently working on learning all different programming
             languages to better suit the development world.  
             Sharpening my skills and continuing my education everyday to prepare myself.
-          Looking to create resposive apps and web pages to easily align with the consumer needs.     </p>
-        </section>     
+            Looking to create resposive apps and web pages to easily align with the consumer needs.     </p>
+        </section>   
+      </div>  
   </div>
 </template>
 
@@ -29,5 +33,28 @@ export default {
     width: 100%;
     height: 100%;
     background: rgb(245, 255, 139);
+}
+
+.homepage-contain {
+  display: flex;
+  
+}
+
+.header {
+  text-align: center;
+  font-size: 2em;
+  padding: 10px;
+}
+
+.images {
+  flex:1;
+  float: left;
+  padding: 5px;
+}
+
+.info {
+  flex:1;
+
+  padding: 5px;
 }
 </style>
