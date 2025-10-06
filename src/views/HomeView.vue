cf270dd621f708e30488a87fca2b6a9ba4316eec
Smash42 <ashley64292@cccneb.edu>
2025-10-06 13:42:59 -0400

    Bulma added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,26 @@
 <template>
-  <h1 class="header"> Ashley Tierney Portfolio</h1>
-  <div class="home backdrop homepage-contain">
-    <div class="images">
-        <section> image Insert
+  <h1 class="header title is-2"> Ashley Tierney Portfolio</h1>
+  <div class="home backdrop homepage-contain columns">
+    <div class="images column is-one-third">
+        <section class="block"> 
+          Image Insert
         </section>
     </div >
-    <div class="info">
-        <section>
-          <h3>About Me: </h3>
-            <p> An aspiring web developer, who is taking the time to go 
+    <div class="info column">
+        <section class="section ">
+          <h3 class="title is-4 has-text-primary-dark">About Me: </h3>
+            <p class="subtitle is-5 has-text-grey-darker"> An aspiring web developer, who is taking the time to go 
             back to school whilst working full time. Diligently working on learning all different programming
             languages to better suit the development world.  
             Sharpening my skills and continuing my education everyday to prepare myself.
             Looking to create resposive apps and web pages to easily align with the consumer needs.     </p>
         </section>   
       </div>  
+  </div>
+  <div class="footer p-2 columns">
+    <section class="column"> Developer in the making </section>
+    <section class="column">  </section>
+
   </div>
 </template>
 
@@ -44,6 +50,7 @@ export default {
   text-align: center;
   font-size: 2em;
   padding: 10px;
+  color: black;
 }
 
 .images {
@@ -53,8 +60,11 @@ export default {
 }
 
 .info {
-  flex:1;
-
+  flex:1;  
   padding: 5px;
+}
+
+.footer {
+  background: rgba(123, 241, 192, 0.288);
 }
 </style>
